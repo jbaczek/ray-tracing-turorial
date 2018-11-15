273ba199b27d9f3b79a8338d7973acd36cba22da
Jan Bączek <jbaczek@nvidia.com>
2018-11-15 16:31:57 +0100

    new files

---
+++ simple-ray-02/ray.h
@@ -0,0 +1,18 @@
+#ifndef RAY_H
+#define RAY_H
+#include "vec3.h"
+
+class ray
+{
+    public:
+        ray() {}
+        ray(const vec3& a, const vec3& b) { A = a; B = b; }
+        inline vec3 origin() const { return A; }
+        inline vec3 direction() const { return B; }
+        vec3 point_at_parameter(float t) { return A + t*B; }
+        
+        vec3 A;
+        vec3 B;
+}
+
+#endif
