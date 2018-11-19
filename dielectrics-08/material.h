874325c5e4d36457f732a1ce94ec6212f22aa7b8
Jan Bączek <jbaczek@nvidia.com>
2018-11-19 16:05:12 +0100

    dielectrics

---
+++ dielectrics-08/material.h
@@ -0,0 +1,79 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+#include "rand_vec.h"
+#include "vec3.h"
+
+class material
+{
+    public:
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
+};
+
+class lambertian : public material
+{
+    public:
+        lambertian(const vec3& a) : albedo(a) {}
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
+        {
+            vec3 target = rec.normal + random_in_unit_sphere();
+            scattered = ray(rec.p, target);
+            attenuation = albedo;
+            return true;
+        }
+
+        vec3 albedo;
+};
+
+class metal : public material
+{
+    public:
+        metal(const vec3& a, float f) : albedo(a) { if (f<1) fuzz = f; else fuzz = 1; }
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
+        {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+            attenuation = albedo;
+            return dot(scattered.direction(), rec.normal) > 0;
+        }
+
+        vec3 albedo;
+        float fuzz;
+};
+
+class dielectric : public material
+{
+    public:
+        dielectric(float ri) : ref_idx(ri) {}
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
+        {
+            vec3 outward_normal;
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            float ni_over_nt;
+            attenuation = vec3(1.0, 1.0, 1.0);
+            vec3 refracted;
+            if(dot(r_in.direction(), rec.normal) > 0)
+            {
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+            }
+            else
+            {
+                outward_normal = rec.normal;
+                ni_over_nt = 1.0 / ref_idx;
+            }
+            if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
+            {
+                scattered = ray(rec.p, reflected);
+            }
+            else
+            {
+                scattered = ray(rec.p, reflected);
+                return false;
+            }
+            return true;
+        }
+        
+        float ref_idx;
+};
+
+#endif
