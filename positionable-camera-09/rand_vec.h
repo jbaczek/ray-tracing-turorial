01f4d985c4365448a56169e9e40f498546ae3fd6
Jan Bączek <jbaczek@nvidia.com>
2018-11-20 14:46:40 +0100

    positionable-camera

---
+++ positionable-camera-09/rand_vec.h
@@ -0,0 +1,21 @@
+#ifndef RAND_VEC_H
+#define RAND_VEC_H
+#include "vec3.h"
+
+float rand_float()
+{
+    return float(rand())/(float(RAND_MAX)+1.0);
+}
+
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do
+    {
+        p = 2.0*vec3(rand_float(), rand_float(), rand_float()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+
+    return p;
+}
+
+#endif
