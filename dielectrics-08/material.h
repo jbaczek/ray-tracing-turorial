bd0f95ea2612bf8563e7f2e7ad36137bde2d4e84
Jan Bączek <jbaczek@nvidia.com>
2018-11-20 13:35:42 +0100

    dielectric

--- dielectrics-08/material.h
+++ dielectrics-08/material.h
@@ -1,7 +1,38 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 #include "rand_vec.h"
-#include "vec3.h"
+#include "ray.h"
+#include "hitable.h"
+
+struct hit_record;
+
+float schlick(float cosine, float ref_idx) 
+{
+    float r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0*r0;
+    return r0 + (1-r0)*pow((1 - cosine),5);
+}
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
+{
+    vec3 uv = unit_vector(v);
+    vec3 un = unit_vector(n);
+    float cosI = -dot(uv, un);
+    float sinT2 = ni_over_nt*ni_over_nt*( 1.0 - cosI * cosI);
+    if (sinT2 < 1.0)
+    {
+        float cosT = sqrt(1.0 - sinT2);
+        refracted = ni_over_nt*uv + (ni_over_nt*cosI - cosT)*un;
+        return true;
+    }
+    else
+        return false;
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2*dot(v,n)*n;
+}
 
 class material
 {
@@ -51,25 +82,30 @@ class dielectric : public material
             float ni_over_nt;
             attenuation = vec3(1.0, 1.0, 1.0);
             vec3 refracted;
+            float reflection_prob;
+            float ang; // it represents sin/cos of an angle
             if(dot(r_in.direction(), rec.normal) > 0)
             {
                 outward_normal = -rec.normal;
                 ni_over_nt = ref_idx;
+                ang = dot(r_in.direction(), rec.normal) / (r_in.direction().length()*rec.normal.length()); //cosine
+                ang = sqrt(1.0 - ref_idx*ref_idx*(1.0-ang*ang)); // sine
             }
             else
             {
                 outward_normal = rec.normal;
                 ni_over_nt = 1.0 / ref_idx;
+                ang = -dot(r_in.direction(), rec.normal) / (r_in.direction().length()*rec.normal.length()); //cosine
             }
+
             if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
-            {
-                scattered = ray(rec.p, reflected);
-            }
+                reflection_prob = schlick(ang, ref_idx);
             else
-            {
+                reflection_prob = 1.0;
+            if (rand_float() < reflection_prob)
                 scattered = ray(rec.p, reflected);
-                return false;
-            }
+            else
+                scattered = ray(rec.p, refracted);
             return true;
         }
         
