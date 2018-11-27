5e21b9167cdcf6a6b9653d13f6284945347572fe
Jan Bączek <jbaczek@nvidia.com>
2018-11-27 15:59:21 +0100

    cuda

--- cuda-12/main.cu
+++ cuda-12/main.cu
@@ -10,6 +10,7 @@
 #include "camera.h"
 #include "material.h"
 
+#define RND curand_uniform(rand_state)
 // limited version of checkCudaErrors from helper_cuda.h in CUDA examples
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 
@@ -26,42 +27,44 @@ __global__ void create_world(hitable** d_list, hitable** d_world, curandState* r
 {
     if(threadIdx.x==0 && blockIdx.x==0)
     {
-        d_list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+        d_list[0] = new sphere(vec3(0,-1000,-1), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
         int i = 1;
         for(int a=-11; a<11; a++)
         {
             for(int b=-11; b<11; b++)
             {
-                float choose_mat = curand_uniform(rand_state);
-                vec3 center(a+0.9f*curand_uniform(rand_state), 0.2f, b+0.9f*curand_uniform(rand_state));
-                if ((center - vec3(4, 0.2, 0)).length() > 0.9)
+                float choose_mat = RND;
+                vec3 center(a+RND, 0.2f, b+RND);
+                if(choose_mat < 0.8f)
                 {
-                    if(choose_mat < 0.8f)
-                    {
-                        d_list[i++] = new sphere(center, 0.2, new lambertian(vec3(curand_uniform(rand_state)*curand_uniform(rand_state), curand_uniform(rand_state)*curand_uniform(rand_state), curand_uniform(rand_state)*curand_uniform(rand_state))));
-                    }
-                    else if(choose_mat < 0.95f)
-                    {
-                        d_list[i++] = new sphere(center, 0.2,
-                                new metal(vec3(0.5*(1+curand_uniform(rand_state)), 0.5*(1+curand_uniform(rand_state)), 0.5*(1+curand_uniform(rand_state))), 0.5*curand_uniform(rand_state)));
-                    }
-                    else
-                    {
-                        d_list[i++] = new sphere(center, 0.2, new dielectric(1.5));
-                    }
+                    d_list[i++] = new sphere(center, 0.2,
+                            new lambertian(vec3(RND*RND, RND*RND, RND*RND)));
+                }
+                else if(choose_mat < 0.95f)
+                {
+                    d_list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5f*(1.0f+RND), 0.5f*(1.0f+RND), 0.5f*(1.0f+RND)), 0.5f*RND));
+                }
+                else
+                {
+                    d_list[i++] = new sphere(center, 0.2, new dielectric(1.5));
                 }
             }
         }
         d_list[i++] = new sphere(vec3(0,1,0), 1.0, new dielectric(1.5));
         d_list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
         d_list[i++] = new sphere(vec3(4,1,0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+        *d_world = new hitable_list(d_list, 22*22+4);
     }
 }
 
 __global__ void free_world(hitable **d_list, hitable **d_world)
 {
-    delete *(d_list);
-    delete *(d_list+1);
+    for(int i=0; i<22*22+4; i++)
+    {
+        delete((sphere*)d_list[i])->mat_ptr;
+        delete d_list[i];
+    }
     delete *d_world;
 }
 
@@ -85,33 +88,39 @@ __global__ void free_camera(camera** cam)
     if(threadIdx.x==0 && blockIdx.x==0)
         delete *(cam);
 }
-__device__ vec3 color(const ray& r, hitable** world, curandState* local_rand_state)
+__device__ vec3 color(const ray& r, hitable** world, curandState* rand_state)
 {
     ray cur_ray = r;
-    ray scattered;
-    vec3 attenuation(1.0, 1.0, 1.0);
-    vec3 cur_attenuation;
+    vec3 cur_attenuation = vec3(1.0, 1.0, 1.0);
     for(int i=0; i<50; i++)
     {
         hit_record rec;
-        if((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec) &&
-                rec.mat_ptr->scatter(cur_ray, rec, cur_attenuation, scattered, local_rand_state))
+        if((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec))
         {
-            attenuation *= cur_attenuation;
-            cur_ray = scattered;
+            ray scattered;
+            vec3 attenuation;
+            if(rec.mat_ptr->scatter(cur_ray, rec, attenuation, scattered, rand_state))
+            {
+                cur_attenuation *= attenuation;
+                cur_ray = scattered;
+            }
+            else
+            {
+                return vec3(0,0,0);
+            }
         }
         else
         {
             vec3 unit_direction = unit_vector(r.direction());
             float t = 0.5f*(unit_direction.y() + 1.0f);
             vec3 c = (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-            return attenuation*c;
+            return cur_attenuation*c;
         }
     }
     return vec3(0,0,0);
 }
 
-__global__ void render_init(int max_x, int max_y, curandState *rand_state)
+__global__ void render_init(int max_x, int max_y, curandState* rand_state)
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
@@ -170,7 +179,8 @@ int main() {
 
     // create world
     hitable **d_list;
-    checkCudaErrors(cudaMalloc((void**) &d_list, 200*sizeof(hitable*))); //allocate sufficient amount of memory to hold whole world
+    int num_hitables =22*22+4;
+    checkCudaErrors(cudaMalloc((void**) &d_list, num_hitables*sizeof(hitable*))); 
     hitable **d_world;
     checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
     create_world<<<1,1>>>(d_list,d_world, d_rand_state);
