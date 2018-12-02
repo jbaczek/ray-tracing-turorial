ed62d1b457d7850b7bfb9d5786a6a0f99f5a975c
jbaczek <jbaczek@nvidia.com>
2018-12-02 18:58:25 +0100

    add motion blur

---
+++ motion-blur-13/main.cu
@@ -0,0 +1,239 @@
+#include <iostream>
+#include <time.h>
+#include <float.h>
+#include <curand_kernel.h>
+#include "vec3.h"
+#include "ray.h"
+#include "hitable.h"
+#include "hitablelist.h"
+#include "sphere.h"
+#include "camera.h"
+#include "material.h"
+
+#define RND curand_uniform(rand_state)
+// limited version of checkCudaErrors from helper_cuda.h in CUDA examples
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+__global__ void create_world(hitable** d_list, hitable** d_world, curandState* rand_state)
+{
+    if(threadIdx.x==0 && blockIdx.x==0)
+    {
+        d_list[0] = new sphere(vec3(0,-1000,-1), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+        int i = 1;
+        for(int a=-11; a<11; a++)
+        {
+            for(int b=-11; b<11; b++)
+            {
+                float choose_mat = RND;
+                vec3 center(a+RND, 0.2, b+RND);
+                if(choose_mat < 0.8f)
+                {
+                    d_list[i++] = new moving_sphere(center, center+vec3(0,0.5*RND,0), 0.0, 1.0, 0.2,
+                            new lambertian(vec3(RND*RND, RND*RND, RND*RND)));
+                }
+                else if(choose_mat < 0.95f)
+                {
+                    d_list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5f*(1.0f+RND), 0.5f*(1.0f+RND), 0.5f*(1.0f+RND)), 0.5f*RND));
+                }
+                else
+                {
+                    d_list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+        d_list[i++] = new sphere(vec3(0,1,0), 1.0, new dielectric(1.5));
+        d_list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+        d_list[i++] = new sphere(vec3(4,1,0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+        *d_world = new hitable_list(d_list, 22*22+4);
+    }
+}
+
+__global__ void free_world(hitable **d_list, hitable **d_world)
+{
+    for(int i=0; i<22*22+4; i++)
+    {
+        delete((sphere*)d_list[i])->mat_ptr;
+        delete d_list[i];
+    }
+    delete *d_world;
+}
+
+__global__ void create_camera(camera** cam, int nx, int ny)
+{
+    if(threadIdx.x==0 && blockIdx.x==0)
+    {
+        vec3 lookfrom(13, 2, 3);
+        vec3 lookat(0.0, 0.0, 0);
+        vec3 vup(0, 1, 0);
+        float vfov = 20.0;
+        float aspect = float(nx)/float(ny);
+        float aperture = 0.0;
+        float focus_dist = (lookat-lookfrom).length();
+        *cam = new camera(lookfrom, lookat, vup, vfov, aspect, aperture, focus_dist, 0.0, 1.0);
+    }
+}
+
+__global__ void free_camera(camera** cam)
+{
+    if(threadIdx.x==0 && blockIdx.x==0)
+        delete *(cam);
+}
+__device__ vec3 color(const ray& r, hitable** world, curandState* rand_state)
+{
+    ray cur_ray = r;
+    vec3 cur_attenuation = vec3(1.0, 1.0, 1.0);
+    for(int i=0; i<50; i++)
+    {
+        hit_record rec;
+        if((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec))
+        {
+            ray scattered;
+            vec3 attenuation;
+            if(rec.mat_ptr->scatter(cur_ray, rec, attenuation, scattered, rand_state))
+            {
+                cur_attenuation *= attenuation;
+                cur_ray = scattered;
+            }
+            else
+            {
+                return vec3(0,0,0);
+            }
+        }
+        else
+        {
+            vec3 unit_direction = unit_vector(cur_ray.direction());
+            float t = 0.5f*(unit_direction.y() + 1.0f);
+            vec3 c = (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+            return cur_attenuation*c;
+        }
+    }
+    return vec3(0,0,0);
+}
+
+__global__ void rand_init(curandState *rand_state) 
+{
+    if (threadIdx.x == 0 && blockIdx.x == 0)
+    {
+        curand_init(1984, 0, 0, rand_state);
+    }
+}
+
+__global__ void render_init(int max_x, int max_y, curandState* rand_state)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
+    curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera ** cam, hitable** world, curandState *rand_state) 
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
+    curandState local_rand_state = rand_state[pixel_index];
+    vec3 col(0,0,0);
+    for(int s=0; s<ns; s++)
+    {
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u,v, &local_rand_state);
+        col += color(r, world, &local_rand_state);
+    }
+    fb[pixel_index] = col/float(ns);
+}
+
+
+int main() {
+    int nx = 2000;
+    int ny = 1000;
+    int ns = 100;
+    int tx = 8;
+    int ty = 8;
+
+    dim3 blocks(nx/tx+1,ny/ty+1);
+    dim3 threads(tx,ty);
+
+    std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = nx*ny;
+    size_t fb_size = num_pixels*sizeof(vec3);
+
+    // allocate FB
+    vec3 *fb;
+    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+
+    // curand init
+    curandState* d_rand_state;
+    checkCudaErrors(cudaMalloc((void**)&d_rand_state, num_pixels*sizeof(curandState)));
+    curandState* d_rand_state2;
+    checkCudaErrors(cudaMalloc((void**)&d_rand_state2, sizeof(curandState)));
+    rand_init<<<1,1>>>(d_rand_state2);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    // create world
+    hitable **d_list;
+    int num_hitables =22*22+4;
+    checkCudaErrors(cudaMalloc((void**) &d_list, num_hitables*sizeof(hitable*))); 
+    hitable **d_world;
+    checkCudaErrors(cudaMalloc((void**) &d_world, sizeof(hitable*)));
+    create_world<<<1,1>>>(d_list,d_world, d_rand_state2);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    //create camera
+    camera **d_cam;
+    checkCudaErrors(cudaMalloc((void**) &d_cam, sizeof(camera*)));
+    create_camera<<<1,1>>>(d_cam, nx, ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    clock_t start, stop;
+    start = clock();
+    // Render our buffer
+    render_init<<<blocks, threads>>>(nx, ny, d_rand_state);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    render<<<blocks, threads>>>(fb, nx, ny, ns, d_cam, d_world, d_rand_state);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "took " << timer_seconds << " seconds.\n";
+
+    // Output FB as Image
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            size_t pixel_index = j*nx + i;
+            int ir = int(255.99*fb[pixel_index].r());
+            int ig = int(255.99*fb[pixel_index].g());
+            int ib = int(255.99*fb[pixel_index].b());
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    checkCudaErrors(cudaDeviceSynchronize());
+    free_world<<<1,1>>>(d_list, d_world);
+    checkCudaErrors(cudaGetLastError());
+    free_camera<<<1,1>>>(d_cam);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaFree(d_list));
+    checkCudaErrors(cudaFree(d_world));
+    checkCudaErrors(cudaFree(d_cam));
+    checkCudaErrors(cudaFree(fb));
+}
---
+++ motion-blur-13/material.h
@@ -0,0 +1,116 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+#include "rand_vec.h"
+#include "ray.h"
+#include "hitable.h"
+#include <curand_kernel.h>
+
+struct hit_record;
+
+__device__ float schlick(float cosine, float ref_idx) 
+{
+    float r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0*r0;
+    return r0 + (1-r0)*pow((1 - cosine),5);
+}
+
+__device__ bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
+{
+    vec3 uv = unit_vector(v);
+    vec3 un = unit_vector(n);
+    float cosI = -dot(uv, un);
+    float sinT2 = ni_over_nt*ni_over_nt*( 1.0 - cosI * cosI);
+    if (sinT2 < 1.0)
+    {
+        float cosT = sqrt(1.0 - sinT2);
+        refracted = ni_over_nt*uv + (ni_over_nt*cosI - cosT)*un;
+        return true;
+    }
+    else
+        return false;
+}
+
+__device__ inline vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2*dot(v,n)*n;
+}
+
+class material
+{
+    public:
+        __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* rand_state) const = 0;
+};
+
+class lambertian : public material
+{
+    public:
+        __device__ lambertian(const vec3& a) : albedo(a) {}
+        __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* rand_state) const
+        {
+            vec3 target = rec.normal + random_in_unit_sphere(rand_state);
+            scattered = ray(rec.p, target, r_in.time());
+            attenuation = albedo;
+            return true;
+        }
+
+        vec3 albedo;
+};
+
+class metal : public material
+{
+    public:
+        __device__ metal(const vec3& a, float f) : albedo(a) { if (f<1) fuzz = f; else fuzz = 1; }
+        __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* rand_state) const
+        {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere(rand_state), r_in.time());
+            attenuation = albedo;
+            return dot(scattered.direction(), rec.normal) > 0;
+        }
+
+        vec3 albedo;
+        float fuzz;
+};
+
+class dielectric : public material
+{
+    public:
+        __device__ dielectric(float ri) : ref_idx(ri) {}
+        __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState* rand_state) const
+        {
+            vec3 outward_normal;
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            float ni_over_nt;
+            attenuation = vec3(1.0, 1.0, 1.0);
+            vec3 refracted;
+            float reflection_prob;
+            float ang; // it represents sin/cos of an angle
+            if(dot(r_in.direction(), rec.normal) > 0)
+            {
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+                ang = dot(r_in.direction(), rec.normal) / (r_in.direction().length()*rec.normal.length()); //cosine
+                ang = sqrt(1.0 - ref_idx*ref_idx*(1.0-ang*ang)); // sine
+            }
+            else
+            {
+                outward_normal = rec.normal;
+                ni_over_nt = 1.0 / ref_idx;
+                ang = -dot(r_in.direction(), rec.normal) / (r_in.direction().length()*rec.normal.length()); //cosine
+            }
+
+            if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
+                reflection_prob = schlick(ang, ref_idx);
+            else
+                reflection_prob = 1.0;
+            if (curand_uniform(rand_state) < reflection_prob)
+                scattered = ray(rec.p, reflected, r_in.time());
+            else
+                scattered = ray(rec.p, refracted, r_in.time());
+            return true;
+        }
+        
+        float ref_idx;
+};
+
+#endif
