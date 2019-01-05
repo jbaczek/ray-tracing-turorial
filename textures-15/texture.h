c12ea8e97244c3db0c7558e2c305c50fcede369e
jbaczek <jbaczek@nvidia.com>
2019-01-05 18:42:46 +0100

    add textures

---
+++ textures-15/texture.h
@@ -0,0 +1,42 @@
+#include "vec3.h"
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+class e_texture
+{
+    public:
+        __device__ virtual vec3 value(float u, float v, const vec3& p) const = 0;
+};
+
+class constant_texture : public e_texture
+{
+    public:
+        __device__ constant_texture() {}
+        __device__ constant_texture(vec3 c) : color(c) {}
+        __device__ virtual vec3 value(float u, float v, const vec3& p) const
+        {
+            return color;
+        }
+
+        vec3 color;
+};
+
+class checker_texture : public e_texture
+{
+    public:
+        __device__ checker_texture() {}
+        __device__ checker_texture(e_texture* o, e_texture* e) : odd(o), even(e) {}
+        __device__ virtual vec3 value(float u, float v, const vec3& p) const
+        {
+            float sines = sin(10*p.x())*sin(10*p.y())*sin(10*p.z());
+            if(sines < 0)
+                return odd->value(u, v, p);
+            else
+                return even->value(u, v, p);
+        }
+
+        e_texture* odd;
+        e_texture* even;
+};
+
+#endif
