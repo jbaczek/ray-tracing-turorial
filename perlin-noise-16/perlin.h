5c50b00c940c56d8a41d96e661dd28e6db397b3d
jbaczek <jbaczek@nvidia.com>
2019-01-06 13:03:18 +0100

    add interpolation to perlin noise

--- perlin-noise-16/perlin.h
+++ perlin-noise-16/perlin.h
@@ -8,6 +8,7 @@
 __device__ float* perlin_generate(curandState* rand_state);
 __device__ void permute(int* p, int n, curandState* rand_state);
 __device__ int* perlin_generate_perm(curandState* rand_state);
+__device__ float trilin_interpolation(float c[2][2][2], float u, float v, float w);
 
 class perlin
 {
@@ -24,11 +25,20 @@ class perlin
             float u = p.x() - floor(p.x());
             float v = p.y() - floor(p.y());
             float w = p.z() - floor(p.z());
-            int i = int(4*p.x()) & 255;
-            int j = int(4*p.y()) & 255;
-            int k = int(4*p.z()) & 255;
+            int i = floor(p.x());
+            int j = floor(p.y());
+            int k = floor(p.z());
+            float c[2][2][2];
+            for(int di=0; di<2; di++)
+                for(int dj=0; dj<2; dj++)
+                    for(int dk=0; dk<2; dk++)
+                        c[di][dj][dk] = ranfloat[
+                                perm_x[(i+di) & 255] ^
+                                perm_y[(j+dj) & 255] ^
+                                perm_z[(k+dk) & 255]
+                        ];
 
-            return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+            return trilin_interpolation(c, u, v, w);
         }
 
         float* ranfloat;
@@ -64,5 +74,19 @@ __device__ int* perlin_generate_perm(curandState* rand_state)
         p[i] = i;
     permute(p, 256, rand_state);
     return p;
+}
+
+__device__ float trilin_interpolation(float c[2][2][2], float u, float v, float w)
+{
+    float accum = 0;
+    for(int i=0; i<2; i++)
+        for(int j=0; j<2; j++)
+            for(int k=0; k<2; k++)
+                accum += (i*u + (1-i)*(1-u))*
+                         (j*v + (1-j)*(1-v))*
+                         (k*w + (1-k)*(1-w))*
+                         c[i][j][k];
+
+    return accum;
 }
 #endif
